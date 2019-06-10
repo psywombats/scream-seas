9575da05ec1b1ae9a8806c8f80e9e4e308ac6f36
psywombats <psy_wombats@wombatrpgs.net>
2019-06-09 22:34:56 -0700

    Fix an odd bug with stats drawer

--- mgn-tactics/Assets/Editor/Drawers/StatSetDrawer.cs
+++ mgn-tactics/Assets/Editor/Drawers/StatSetDrawer.cs
@@ -12,27 +12,17 @@ public class StatSetDrawer : PropertyDrawer {
         GUIStyle fieldStyle = new GUIStyle(GUI.skin.textArea);
         fieldStyle.fixedWidth = 80;
 
-        EditorGUI.LabelField(
-            new Rect(pos.x, 6 + pos.y, pos.width, pos.height),
-            "Stats");
+        EditorGUILayout.LabelField("Stats");
 
         for (int i = 0; i < keys.arraySize; i += 1) {
             SerializedProperty serializedValue = values.GetArrayElementAtIndex(i);
             Stat stat = Stat.Get(keys.GetArrayElementAtIndex(i).enumValueIndex);
             float value = serializedValue.floatValue;
             
-            EditorGUI.LabelField(
-                new Rect(18 + pos.x, 24 + pos.y + i * 18, pos.width, pos.height),
-                stat.nameShort + ": ");
             if (stat.useBinaryEditor) {
-                serializedValue.floatValue = EditorGUI.Toggle(
-                    new Rect(pos.x + 100, 24 + pos.y + i * 18, pos.width, pos.height),
-                    (value > 0.0f)) ? 1.0f : 0.0f;
+                serializedValue.floatValue = EditorGUILayout.Toggle(stat.nameShort, value > 0) ? 1.0f : 0.0f;
             } else {
-                serializedValue.floatValue = EditorGUI.FloatField(
-                    new Rect(pos.x + 100, 24 + pos.y + i * 18, pos.width, pos.height),
-                    value,
-                    fieldStyle);
+                serializedValue.floatValue = EditorGUILayout.FloatField(stat.nameShort, value);
             }
 
         }
